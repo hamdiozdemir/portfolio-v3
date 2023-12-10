8af87f1e16fb57b1ae67f1aa65d82f6c56196e72
hamdiozdemir <hamdiozdemir61@gmail.com>
2023-12-10 17:37:56 +0300

    linting fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for backend project.
 """
-from django.conf import settings
+
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -18,11 +18,3 @@ urlpatterns = [
          ),
     path('api/', include('core.urls')),
 ]
-
-
-# if settings.DEBUG:
-#     from django.conf.urls.static import static
-#     urlpatterns += static(settings.STATIC_URL,
-#                           document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
